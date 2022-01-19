03b2adf800037fe1b5c15e91c7bb760f9eb9dcbf
RunjieXie <83536703+RunjieXie@users.noreply.github.com>
2022-01-19 02:32:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("go_to_submission").onclick = function () {
+    location.href = "Submission.html";
+};
+
+document.getElementById("go_to_voting").onclick = function () {
+    location.href = "Voting.html";
+};
